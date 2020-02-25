ac4c893747d6d7da03165bef9af3e1045659c7b3
max-lian <maksimilian199@gmail.com>
2020-02-25 16:35:12 +0200

    Ver 2.0

--- Fractions.py
+++ Fractions.py
@@ -1,20 +1,34 @@
-import NskNsd
+from NskNsd import NskNsd
 class Fractions(object):
     def __init__(self, top: int, bottom: int):
         if(bottom == 0):
             raise ZeroDivisionError
-        self.top = int(top)
-        self.bottom = int(bottom)
+        self.top = top
+        self.bottom = bottom
         Fractions.simplify(self)
 
     def __add__(self, otherfraction):
-        newBottom = NskNsd.NskNsd.nsk(self.bottom, otherfraction.bottom)
+        newBottom = NskNsd.nsk(self.bottom, otherfraction.bottom)
         newTop = self.top*(newBottom//self.bottom) + otherfraction.top*(newBottom//otherfraction.bottom)
         return Fractions(newTop, newBottom)
 
     def __sub__(self, otherfraction):
-        newBottom = NskNsd.NskNsd.nsk(self.bottom, otherfraction.bottom)
+        newBottom = NskNsd.nsk(self.bottom, otherfraction.bottom)
         newTop = self.top * (newBottom // self.bottom) - otherfraction.top * (newBottom // otherfraction.bottom)
+        return Fractions(newTop, newBottom)
+
+    def __mul__(self, otherfraction):
+        nsd1 = NskNsd.nsd(self.top, otherfraction.bottom)
+        nsd2 = NskNsd.nsd(otherfraction.top, self.bottom)
+        newTop = (self.top//nsd1)*(otherfraction.top//nsd2)
+        newBottom = (otherfraction.bottom // nsd1) * (self.bottom // nsd2)
+        return Fractions(newTop, newBottom)
+
+    def __truediv__(self, otherfraction):
+        nsd1 = NskNsd.nsd(self.top, otherfraction.top)
+        nsd2 = NskNsd.nsd(otherfraction.bottom, self.bottom)
+        newTop = (self.top//nsd1)*(otherfraction.bottom//nsd2)
+        newBottom = (otherfraction.top // nsd1) * (self.bottom // nsd2)
         return Fractions(newTop, newBottom)
 
     def __str__(self):
@@ -23,14 +37,15 @@ class Fractions(object):
         return str(self.top//self.bottom)
 
     def simplify(self):
-        nsd = NskNsd.NskNsd.nsd(self.top, self.bottom)
+        nsd = NskNsd.nsd(self.top, self.bottom)
         if(self.bottom < 0): nsd = -nsd
         self.top = self.top//nsd
         self.bottom = self.bottom//nsd
+
 if __name__ == "__main__":
     try:
-        a = Fractions(1, 2)
-        b = Fractions(4, 8)
-        print(a + b)
+        a = Fractions(1, 3)
+        b = Fractions(-2, 9)
+        print(a / b)
     except ZeroDivisionError as err:
         print("Убери с знаменателя 0, дебил")
--- Tests/FractionsTest.py
+++ Tests/FractionsTest.py
@@ -1,6 +1,7 @@
 import unittest
 from Fractions import Fractions
 class FractionsTest(unittest.TestCase):
+
     def  test_baseFractionsFunctions(self):
         a = Fractions(2, 6)
         b = Fractions(2, -3)
@@ -9,7 +10,7 @@ class FractionsTest(unittest.TestCase):
 
     def test_zeroFraction(self):
         a = Fractions(0, 2)
-        b = Fractions('a', 2)
+        self.assertEqual('0', str(a))
 
     def test_baseSumTest(self):
         a = Fractions(1, 3)
@@ -18,5 +19,29 @@ class FractionsTest(unittest.TestCase):
         self.assertEqual('5/9', str(a + b))
         self.assertEqual('47/6', str(a + c))
         self.assertEqual('139/18', str(b + c))
+
+    def test_advansedSumTest(self):
+        a = Fractions(-1, 3)
+        b = Fractions(0, 9)
+        c = Fractions(-5, -2)
+        self.assertEqual('-1/3', str(a + b))
+        self.assertEqual('13/6', str(a + c))
+        self.assertEqual('5/2', str(b + c))
+
+    def test_multTest(self):
+        a = Fractions(1, 3)
+        b = Fractions(-2, 9)
+        c = Fractions(15, 2)
+        self.assertEqual('-2/27', str(a * b))
+        self.assertEqual('5/2', str(a * c))
+        self.assertEqual('-5/3', str(b * c))
+
+    def test_divisionTest(self):
+        a = Fractions(1, 3)
+        b = Fractions(-2, 9)
+        c = Fractions(15, 2)
+        self.assertEqual('-3/2', str(a / b))
+        self.assertEqual('2/45', str(a / c))
+        self.assertEqual('-4/135', str(b / c))
 if __name__ == '__main__':
     unittest.main()
